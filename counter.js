3c819baf6e2923ff7ba1243dd4228a2a702bd829
amusitae <amani.fattani@gmail.com>
2025-10-17 14:01:03 +0700

    aa

--- counter.js
+++ counter.js
@@ -1,13 +1,16 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+  localStorage.setItem('counter', 0);
+}
 
        function count(){
+        let counter = localStorage.getItem('counter');
         counter++;
         document.querySelector('h1').innerHTML = counter;
-
-        if(counter % 10 === 0){
-            alert(`count is now ${counter}`);
-        }
+        localStorage.setItem('counter', counter);
        }
-       document.addEventListener('DOMContentloaded', function(){
+
+       document.addEventListener('DOMContentLoaded', function(){
          document.querySelector('button').onclick = count;
+
+         setInterval(count, 1000);
        });
